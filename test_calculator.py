36f98adb9b63900eb35d8f6f179f9ffdb31583cc
mike <mgarretson@nextgen.com>
2019-01-17 15:30:26 -0500

    fix PEP8 issue

--- test_calculator.py
+++ test_calculator.py
@@ -12,12 +12,11 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(4, calculator.add(2, 2))
 
     def test_subtraction(self):
-        self.assertEqual(2, calculator.subtract(4,2))
+        self.assertEqual(2, calculator.subtract(4, 2))
 
     def test_multiply(self):
         self.assertEqual(16, calculator.multiply(8, 2))
 
     def test_divide(self):
         self.assertEqual(8, calculator.divide(16, 2))
-        self.assertIsNone(calculator.divide(16,0))
-
+        self.assertIsNone(calculator.divide(16, 0))
