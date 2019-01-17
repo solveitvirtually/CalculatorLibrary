0a42ad5b01adabb46d7b661d27ef4858a04a487d
mike <mgarretson@nextgen.com>
2019-01-17 15:09:10 -0500

    work on divide function

--- calculator.py
+++ calculator.py
@@ -13,3 +13,12 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def divide(first_term, second_term):
+    value = None
+    try:
+        value = first_term / second_term
+    except ZeroDivisionError:
+        value = None
+    return value
--- test_calculator.py
+++ test_calculator.py
@@ -3,15 +3,21 @@ Unit tests for calculator library
 """
 
 import calculator
+import unittest
 
 
-class TestCalculator:
+class TestCalculator(unittest.TestCase):
 
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+        self.assertEqual(4, calculator.add(2, 2))
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        self.assertEqual(2, calculator.subtract(4,2))
 
     def test_multiply(self):
-        assert 16 == calculator.multiply(8, 2)
+        self.assertEqual(16, calculator.multiply(8, 2))
+
+    def test_divide(self):
+        self.assertEqual(8, calculator.divide(16, 2))
+        self.assertIsNone(calculator.divide(16,0))
+
